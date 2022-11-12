eb83d1233864f5d72bf287143e4ec9ad6fba8465
Riya <kattelriya4@gmail.com>
2022-11-12 08:29:39 +0545

    Built TIC TAC TOE

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from 'react'
+import './App.css'
+import Header from './components/header'
+import Game from './components/Game'
+
+function App() {
+
+  return (
+    <div className="App">
+    <Game/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(props)
+{
+    return(
+        <div className="" style={{backgroundColor: props.bgColor}}>
+        {props.title}
+        </div>
+    )
+}
+export default Header;
