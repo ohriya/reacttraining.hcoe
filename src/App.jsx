0a7a7d034e9fdc3594e708c6a0dd04ff79aa7f24
Riya <kattelriya4@gmail.com>
2022-11-26 09:39:25 +0545

    react router intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { Fragment, useState } from 'react'
-import './App.css'
-import Header from './components/header'
-import Game from './components/Game'
+import { Link,Outlet,Route, Routes } from "react-router-dom";
+import Game from "./components/Game";
 
-function App() {
-
+export default function App() {
+  
   return (
-    <div className="App">
-    <Game/>
-    </div>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/game">Game</Link></li>
+        </ul>
+      </nav>
+      <Routes> 
+          <Route path ="/" element ={<Home/>}>
+            <Route path="/posts" element={<Post/>}/>
+            <Route path ="/game" element={<Game/>} />   
 
-  )
+          </Route>
+         
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+function Home(){
+
+return(
+  <>
+    <div>Header</div>
+    <div>side bar</div>
+    <Outlet/>
+    <div>Footer</div>
+   </>
+  
+
+);
+}
+
+function Post()
+{
+return(
+  <div>my posts</div>
+);
+}
+
